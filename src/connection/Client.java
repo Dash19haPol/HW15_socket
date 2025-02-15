c3a7a19178f183fc32fc7765d1927652779815d9
Dash19haPol <polovnevadd@gmail.com>
2025-02-15 18:58:43 +0200

    HW15_socket_master_2

--- src/connection/Client.java
+++ src/connection/Client.java
@@ -1,4 +1,51 @@
 package connection;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+import static connection.Server.REASON_TO_STOP;
+import static connection.Server.STOP_WORD;
+
 public class Client {
+    static int PORT = 8081;
+    static String HOSTNAME = "localhost";
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.startClient();
+    }
+
+    void startClient() {
+        try (Socket socket = new Socket(HOSTNAME, PORT)) {
+            System.out.println("Connected to the server at " + HOSTNAME + ";" + PORT);
+            System.out.println("To interrupt communication send message \"" + STOP_WORD + "\"");
+
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            boolean isContinue;
+            do {
+                isContinue = chatWithServer(reader, writer);
+            } while (isContinue);
+
+            reader.close();
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean chatWithServer(BufferedReader reader, PrintWriter writer) throws IOException {
+        Scanner console = new Scanner(System.in);
+        String messageToServer = console.nextLine();
+        System.out.println("Sending to server: " + messageToServer);
+        writer.println(messageToServer);
+        String serverResponse = reader.readLine();
+        System.out.println("Server says: " + serverResponse);
+        return !(STOP_WORD.equalsIgnoreCase(serverResponse) || REASON_TO_STOP.equalsIgnoreCase(serverResponse));
+    }
 }
